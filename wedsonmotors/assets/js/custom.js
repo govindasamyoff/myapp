68c31c8b6138d346747ffefc196a8e503be826ea
Govind <govindasamy@billionapps.net>
2022-12-18 21:55:51 +0530

    new update

--- wedsonmotors/assets/js/custom.js
+++ wedsonmotors/assets/js/custom.js
@@ -1,3 +1,11 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 var App = (function(window){
   "use strict";
   var _this = null;
